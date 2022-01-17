7c669bf6f62813661d78af33f1cebf4660a5baf4
siged <siged@mail.com>
2022-01-17 17:42:16 +0200

    word from random number added

---
+++ testing/src/executorService/WordFromRandomNumberCall.java
@@ -0,0 +1,42 @@
+package executorService;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class WordFromRandomNumberCall {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Future future = executorService.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return randomWordGenerator();
+            }
+        });
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    public static String randomWordGenerator() {
+        Random random = new Random();
+        char letter = 0;
+        String word = "";
+
+        while (word.length() < 10) {
+
+
+            letter = (char) random.nextInt(122);
+            if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z') {
+                word += String.valueOf(letter);
+            }
+
+        }
+        return word;
+    }
+
+}
---
+++ testing/src/executorService/WordFromRandomNumbersES.java
@@ -0,0 +1,36 @@
+package executorService;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WordFromRandomNumbersES {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(randomWordGenerator());
+            }
+        });
+        executorService.shutdown();
+    }
+
+    public static String randomWordGenerator() {
+        Random random = new Random();
+        char letter = 0;
+        String word = "";
+
+        while (word.length() < 10) {
+
+
+                letter = (char) random.nextInt(122);
+                if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z') {
+                    word += String.valueOf(letter);
+                }
+
+        }
+        return word;
+    }
+}
